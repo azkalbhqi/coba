284c6a908697cc7a68704e41d5a0af1ddacdc0bb
azk <azkalbhqi@gmail.com>
2024-04-08 00:48:41 +0700

    nyoh

---
+++ src/utpAzkal/Pelanggan.java
@@ -0,0 +1,16 @@
+package utpAzkal;
+public class Pelanggan {
+    private String nama;
+
+    public Pelanggan(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
